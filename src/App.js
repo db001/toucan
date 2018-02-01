c1c1dc6818321cf744efc530de297c190651c210
db001 <dnbennett@hotmail.co.uk>
2018-02-01 12:58:39 +0000

    Changed data and refactored Post component to suit

--- src/App.js
+++ src/App.js
@@ -4,41 +4,39 @@ import NewPostModal from './components/NewPostModal';
 
 /* Sample data */
 const sampleData = {
-  posts: [
-    {
-      date: '2017-10-13T21:55:25.725Z',
-      headline: 'Post Headline 1',
-      text_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      comments: [
-        'Comment comment comment 1',
-        'Really long but pointless comment to take up space',
-        'Agreed'        
-      ]
-    },
-    {
-      date: '2017-10-09T06:23:47.725Z',
-      headline: 'Post Headline 2',
-      text_content: 'Lorem Ipsum Sequel: The revenge.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      comments: [
-        'Preferred the original',
-        'Is this a trilogy?',       
-      ]
-    },
-    {
-      date: '2017-04-01T12:23:47.725Z',
-      headline: 'No Comment',
-      text_content: 'Dull dull dull...',
-      comments: []
-    },
-    {
-      date: '2016-05-04T12:01:59.001Z',
-      headline: 'Post Headline Number 4',
-      text_content: 'I\'m only here to check that the alternating colour scheme is working...',
-      comments: [
+  post1: {
+    date: '2017-10-13T21:55:25.725Z',
+    headline: 'Post Headline 1',
+    text_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    comments: [
+      'Comment comment comment 1',
+      'Really long but pointless comment to take up space',
+      'Agreed'        
+    ]
+  },
+  post2: {
+    date: '2017-10-09T06:23:47.725Z',
+    headline: 'Post Headline 2',
+    text_content: 'Lorem Ipsum Sequel: The revenge.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    comments: [
+      'Preferred the original',
+      'Is this a trilogy?',       
+    ]
+  },
+  post3: {
+    date: '2017-04-01T12:23:47.725Z',
+    headline: 'No Comment',
+    text_content: 'Dull dull dull...',
+    comments: []
+  },
+  post4: {
+    date: '2016-05-04T12:01:59.001Z',
+    headline: 'Post Headline Number 4',
+    text_content: 'I\'m only here to check that the alternating colour scheme is working...',
+    comments: [
         'Doing a great job bro!',       
-      ]
-    },
-  ]
+    ]
+  }
 };
 
 class App extends Component {
@@ -58,8 +56,9 @@ class App extends Component {
     })
   }
 
+  // Needs refactoring due to change in data
   deletePost = id => {
-    const posts = this.state.sampleData.posts;
+    const posts = this.state.sampleData;
     const matchId = item => item.date !== id;
     const updatedPosts = posts.filter(matchId);
     this.setState({
--- src/components/Post.js
+++ src/components/Post.js
@@ -41,33 +41,33 @@ class Post extends Component {
             </i>new post
           </div>
         </header>
-        { data.posts.map(post => {
+        { Object.keys(data).map(post => {
           return (
             <div
-              key={`post-${post.date}`}
+              key={`post-${data[post].date}`}
               className="post"
             >
               <div className="post-container">
                 <div className="date">
                   <div>
                     <div className="date-day">
-                      {convertDate(post.date).day}. 
+                      {convertDate(data[post].date).day}. 
                     </div>
                     <div className="date-month">
-                      {convertDate(post.date).month}              
+                      {convertDate(data[post].date).month}              
                     </div>
                   </div>              
                 </div>
                 
                 <div className="post-content">
                   <div className="post-body">             
-                      <h3>{post.headline}</h3>                
+                      <h3>{data[post].headline}</h3>                
                       <p className="post-text">
-                        {post.text_content}
+                        {data[post].text_content}
                       </p>                
                   </div>
                   <div className="button-container">
-                      <div onClick={() => deletePost(post.date)}>
+                      <div onClick={() => deletePost(data[post].date)}>
                         <i className="fa fa-times" aria-hidden="true"></i>
                       </div>
                       <div>  
@@ -78,15 +78,14 @@ class Post extends Component {
               </div>
 
               <div className="comment-container">
-                {post.comments.length > 0 ?
-                  post.comments.map((comment, index) => {
+                  {data[post].comments.map((comment, index) => {
                     return (
                       <Comment
                         key={index}
                         comment={comment}
                       />
                     ) 
-                  }) : null
+                  })
                 }
               </div>
             </div>
